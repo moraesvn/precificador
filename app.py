abdbd434702602841d970c0aad19794483939253
Vinicius Moraes <vini.moraes1206@gmail.com>
2026-01-19 15:04:32 -0300

    Teste

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 st.set_page_config(page_title="Callback Tiny V3", page_icon="🔐")
 
-st.title("Callback Tiny • OAuth V3")
-st.write("Se você chegou aqui via Tiny, deve existir `code` e `state` na URL.")
+st.title('Callback Tiny')
+st.write("Deve existir `code` e `state` na URL.")
 
 params = st.query_params  # Streamlit Cloud já traz os params
 st.json(dict(params))
